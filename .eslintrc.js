3f4a04da5653ad9d59383f26962a5a02c39ee337
LuckyWind_sck <luckywindsck@gmail.com>
2021-06-29 10:37:31 +0900

    build: set eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,42 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    /* Console & Debugger */
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    /* TypeScript */
+    // Fix ESLint error of "Cannot read property 'loc' of undefined."
+    // https://github.com/typescript-eslint/typescript-eslint/issues/792#issuecomment-517762395
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2],
+
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn',
+      {
+        varsIgnorePattern: '_',
+        argsIgnorePattern: '_',
+      },
+    ],
+
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/type-annotation-spacing': 'error',
+    '@typescript-eslint/member-delimiter-style': 'error',
+
+    /* Import */
+    'import/order': ['error',
+      {
+        alphabetize: { order: 'asc' },
+        'newlines-between': 'always',
+      },
+    ],
+    'sort-imports': ['error',
+      { ignoreDeclarationSort: true },
+    ],
+
+    /* Other */
+    'no-shadow': ['error',
+      { allow: ['_'] },
+    ],
   },
 };
